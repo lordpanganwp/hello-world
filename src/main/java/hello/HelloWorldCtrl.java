55472b3d6528c1bf34f409671b84914d4bbcb596
Lord Pangan <lord.pangan@webbfontaine.com>
2021-06-28 22:17:22 +0800

    update properties

--- src/main/java/hello/HelloWorldCtrl.java
+++ src/main/java/hello/HelloWorldCtrl.java
@@ -9,7 +9,7 @@ public class HelloWorldCtrl {
     @RequestMapping("/")
     public String index() {
         String prefix = System.getenv().getOrDefault("ENV_PREFIX", "Blank");
-        return "<h1 style='background-color:tomato;width: 100%;height: 100%;text-align: center;'> 'Greetings from "+prefix+"' </h1>";
+        return "<h1 style='background-color:powderblue;width: 100%;height: 100%;text-align: center;'> 'Greetings from "+prefix+"' </h1>";
     }
 
 }
